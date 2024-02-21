29db767a85bc02b0d40f22301d563cbb0fa88f54
Jeaner Silva <jeaner.silva@abingov.onmicrosoft.com>
2024-02-21 20:43:21 -0300

    gemini resumir

--- gemini_resumo.py
+++ gemini_resumo.py
@@ -2,7 +2,6 @@ from secret import apikey, safety_settings
 import fileinput
 import google.generativeai as genai
 import io
-import linecache
 
 genai.configure(api_key=apikey)
 
@@ -27,18 +26,17 @@ def gerar_respostas(arquivo_perguntas_name, arquivo_respostas_name):
     with io.open(arquivo_respostas_name, 'w', encoding='utf-8') as arquivo_respostas:
         arquivo_respostas.close()
 
-    with open(arquivo_perguntas_name, 'r', encoding='utf-8') as arquivo_perguntas:
-            total_linhas = len(arquivo_perguntas.readlines())
-
     with io.open(arquivo_perguntas_name, 'r', encoding='utf-8') as arquivo_perguntas:
         temas = arquivo_perguntas.read().split("Item do edital:")    
         for conteudo in temas:
-            print(f"conteudo {conteudo}")
+            titulo = conteudo.split("\n")[0]
+            print(f"Resumindo {titulo.strip()}...")            
             with io.open(arquivo_respostas_name, 'a', encoding='utf-8') as arquivo_respostas:
                 prompt_parts[0] = f"input: resuma {conteudo} em um parágrafo. Se houver fórmulas, cite-as. See houver subtópicos, liste-os"
                 response = model.generate_content(prompt_parts)            
-                arquivo_respostas.write(f"{response.text.strip()}\n\n")            
+                arquivo_respostas.write(f"**{titulo.strip()}**: {response.text.strip()}\n\n")            
+                
 
-arquivo_respostas_name = "completo_resumo.txt"
-arquivo_perguntas_name = "saida/completo2.txt"
+arquivo_respostas_name = "saida/completo_resumo.txt"
+arquivo_perguntas_name = "saida/completo.txt"
 gerar_respostas (arquivo_perguntas_name, arquivo_respostas_name)
