d339ac54dac7d7e592a931b8f10d4ce17d2520d1
JeanerSilva <jeanerufmg@gmail.com>
2024-03-10 10:35:43 -0300

    Nova rodada

--- gemini.py
+++ gemini.py
@@ -2,7 +2,7 @@ from secret import apikey, safety_settings
 import fileinput
 import google.generativeai as genai
 import io
-import linecache
+from datetime import datetime
 
 genai.configure(api_key=apikey)
 
@@ -22,23 +22,32 @@ def gerar_respostas(arquivo_perguntas_name, arquivo_respostas_name):
     "input: ",
     "output: "
     ]
-    with io.open(arquivo_respostas_name, 'w', encoding='utf-8') as arquivo_respostas:
-        arquivo_respostas.close()
 
     with open(arquivo_perguntas_name, 'r', encoding='utf-8') as arquivo_perguntas:
             total_linhas = len(arquivo_perguntas.readlines())
+    arquivo_perguntas.close()
 
+    timestamp = datetime.now()
+    string_format = timestamp.strftime("%Y-%m-%d-%H-%M-%S_")
+    dir = arquivo_respostas_name.split("/")[0] + "/"    
+    arquivo_para_gravar = dir + string_format + arquivo_respostas_name.split("/")[1] 
     with io.open(arquivo_perguntas_name, 'r', encoding='utf-8') as arquivo_perguntas:
         for indice, pergunta in enumerate(arquivo_perguntas, start=1):
             print(f"Gerando pergunta {indice} de {total_linhas}: {pergunta}")
-            with io.open(arquivo_respostas_name, 'a', encoding='utf-8') as arquivo_respostas:
-                #prompt_parts[0] = f"input: resuma {pergunta.strip()} em tres parágrafos. Se houver fórmulas, cite-as."
-                prompt_parts[0] = f"input: Discorra de forma concisa mas abrangente sobre {pergunta.strip()}. Se houver fórmulas, cite-as."
+    
+            with io.open(arquivo_para_gravar, 'a', encoding='utf-8') as arquivo_respostas:
+                prompt_parts[0] = f"input: Discorra de forma concisa sobre tópicos envolvendo {pergunta.strip()}."
+                response = model.generate_content(prompt_parts)            
+                arquivo_respostas.write(f"Item do edital: {pergunta.strip()}::\n{response.text.strip()}\n\n")            
+                #arquivo_respostas.write(f"Item do edital: {pergunta}\n response.text.strip()\n\n")            
+
+
+arquivo_respostas_name = "saida/respostas.txt"
+arquivo_perguntas_name = "entrada/perguntas.txt"
+gerar_respostas (arquivo_perguntas_name, arquivo_respostas_name)
+
+
+
                 #prompt_parts[0] = f"input: você é especialista em provas do concurso da banca cesgranrio. Então resuma {pergunta.strip()} em um parágrafo."
                 #prompt_parts[0] = f"input: você é especialista. Então resuma {pergunta.strip()} em um parágrafo."
-                response = model.generate_content(prompt_parts)            
-                arquivo_respostas.write(f"Item do edital: {pergunta}\n\n{response.text.strip()}\n\n")            
-
-arquivo_respostas_name = "respostas.txt"
-arquivo_perguntas_name = "perguntas.txt"
-gerar_respostas (arquivo_perguntas_name, arquivo_respostas_name)
+                #prompt_parts[0] = f"input: resuma {pergunta.strip()} em tres parágrafos. Se houver fórmulas, cite-as."
